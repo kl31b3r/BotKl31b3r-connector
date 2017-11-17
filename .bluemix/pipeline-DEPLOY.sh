ce9916c491b8cc48987f9af8e6f7dbacd53e2ed7
kl31b3r <kleiber.huiman@itsolutioncorp.com>
2017-11-17 10:43:43 -0500

    Update pipeline-DEPLOY.sh

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -22,11 +22,11 @@ echo 'KMHA: Until SPACE_KEY...'
 echo "KMHA: CLOUDFUNCTIONS_KEYS..${CLOUDFUNCTIONS_KEYS}"
 echo "KMHA: CF_ORG..${CF_ORG}"
 echo "KMHA: CF_SPACE..${CF_SPACE}"
-SPACE_KEY=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == "'${CF_ORG}'_'$CF_SPACE'") | .key'`
+SPACE_KEY=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == "$CF_ORG"_"$CF_SPACE") | .key'`
 echo "KMHA: ${SPACE_KEY}"
 
 echo 'KMHA: Until SPACE_UUID...'
-SPACE_UUID=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == "'${CF_ORG}'_'$CF_SPACE'") | .uuid'`
+SPACE_UUID=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == \"$CF_ORG'_'$CF_SPACE\") | .uuid'`
 echo 'KMHA:' ${SPACE_UUID}
 
 echo 'KMHA: UntilCLOUDFUNCTIONS_AUTH...'
