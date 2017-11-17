ca726d69b59441c29b5156de4f154001b58707d0
kl31b3r <kleiber.huiman@itsolutioncorp.com>
2017-11-17 10:53:51 -0500

    Update pipeline-DEPLOY.sh

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -22,11 +22,13 @@ echo 'KMHA: Until SPACE_KEY...'
 echo "KMHA: CLOUDFUNCTIONS_KEYS..${CLOUDFUNCTIONS_KEYS}"
 echo "KMHA: CF_ORG..${CF_ORG}"
 echo "KMHA: CF_SPACE..${CF_SPACE}"
-SPACE_KEY=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == "$CF_ORG"_"$CF_SPACE") | .key'`
+CF_UNION="$CF_ORG"_"$CF_SPACE"
+echo "KMHA: CF_UNION..${CF_UNION}"
+SPACE_KEY=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == "'$CF_UNION'") | .key'`
 echo "KMHA: ${SPACE_KEY}"
 
 echo 'KMHA: Until SPACE_UUID...'
-SPACE_UUID=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == \"$CF_ORG'_'$CF_SPACE\") | .uuid'`
+SPACE_UUID=`echo $CLOUDFUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == \"$CF_ORG\"_\"$CF_SPACE\") | .uuid'`
 echo 'KMHA:' ${SPACE_UUID}
 
 echo 'KMHA: UntilCLOUDFUNCTIONS_AUTH...'
